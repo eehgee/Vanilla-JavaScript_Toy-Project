2ac986328d1e381f61fa8722865a3911bb1c4fb2
Eunju Kim <wnqrx@naver.com>
2024-05-08 19:22:18 +0900

    24_0508_commit

---
+++ Infinite Scroll/scroll.js
@@ -0,0 +1,64 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const get = (target) => {
+    return document.querySelector(target);
+  };
+
+  let page = 1;
+  let total = 10;
+  const end = 10;
+  let loading = false;
+  let loaded = get('.loading')
+  let container = get('.container')
+
+
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry=>{
+        if(entry.isIntersecting && !loading){
+            loading = true
+            datapull()
+
+        }
+    })
+  }, {rootMargin: "0px", threshold: 0.1})
+
+  observer.observe(loaded)
+
+  async function datapull(){
+    try {
+        const api = await fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${total}`)
+        const response = await api.json()
+        renderdata(response)
+        
+        page++
+        loading = false;
+        if(page <= end){
+            container.appendChild(loaded)
+        }else{
+            observer.disconnect()
+            loaded.style.display = 'none';
+        }
+
+
+    } catch (error) {
+        console.log('error')
+        loading = false;
+    }
+  }
+
+  function renderdata(response){
+    response.forEach((item) => {
+        let itemBox = document.createElement('div')
+        itemBox.classList.add('item')
+        itemBox.innerHTML = 
+        `<div class="header">
+            <div class="id">${item.id}</div>    
+            <div class="title">${item.title}</div>    
+        </div>
+        <div class="body">
+            <div class="contents">${item.body}</div>
+        </div>`;
+        container.appendChild(itemBox)
+    });
+  }
+
+});
--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 ## 3D crousel
 
 # rotateY 값 구하기
-ex. item이 총 6개 => 360 / 6
+ex. item이 총 6개 => 360 / 6 = 60
 
 # translateZ 값 구하기
+ex. 지름 : 300px, 각도 : 60
+지름의 반 : 150px
+각도의 반 : 30도
+150/tan(30도) : 259
 
-r값 : https://3dtransforms.desandro.com/img/diagram.png
-탄젠트 방정식 : https://3dtransforms.desandro.com/img/calc.png
+# perspective
+원근감을 조절하여 3D효과를 만들 때 사용
+
+
+
+## Infinite Scroll
+
+# 100개 데이터 받아오기
+https://jsonplaceholder.typicode.com/posts
+
+
